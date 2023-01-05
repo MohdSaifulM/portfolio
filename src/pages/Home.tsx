fe6d96f00b785b9bb50834d2d03fb99609243901
SaifM <mohd.saiful@live.com>
2023-01-06 02:51:14 +0800

    style: 💄 Added circles for the git branch design

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,14 +2,20 @@ const Home = () => {
     return (
         <main>
             <section className="section-tree">
-                <div className="branch-row">
-                </div>
-                <div className="branch-row">
-                    <div className="circle-group">
+                <div className="branch-row branch-row--top">
+                <div className="circle-group circle-group--top">
+                         <div className="circle"></div>
+                         <div className="circle"></div>
                          <div className="circle"></div>
                          <div className="circle"></div>
                     </div>
-                    <div className="circle-group">
+                </div>
+                <div className="branch-row branch-row--bottom">
+                    <div className="circle-group circle-group--bottom">
+                         <div className="circle"></div>
+                         <div className="circle"></div>
+                    </div>
+                    <div className="circle-group circle-group--bottom">
                         <div className="circle"></div>
                         <div className="circle"></div>
                     </div>
