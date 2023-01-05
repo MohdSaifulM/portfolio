5166720dbb62be2ca7bf9d255e613a23b50da9e1
SaifM <mohd.saiful@live.com>
2023-01-06 00:55:44 +0800

    style: 💄 Added some html for the git branch design

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,18 @@ const Home = () => {
     return (
         <main>
             <section className="section-tree">
-
+                <div className="branch-row">
+                </div>
+                <div className="branch-row">
+                    <div className="circle-group">
+                         <div className="circle"></div>
+                         <div className="circle"></div>
+                    </div>
+                    <div className="circle-group">
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                    </div>
+                </div>
             </section>
         </main>
     )
