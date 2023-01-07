9843794f8c7ecd83c2a0b637401505af89086000
SaifM <mohd.saiful@live.com>
2023-01-07 17:51:32 +0800

    fix: 🐛 Rethink design of homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,28 +1,5 @@
 const Home = () => {
-    return (
-        <main>
-            <section className="section-tree">
-                <div className="branch-row branch-row--top">
-                <div className="circle-group circle-group--top">
-                         <div className="circle"></div>
-                         <div className="circle"></div>
-                         <div className="circle"></div>
-                         <div className="circle"></div>
-                    </div>
-                </div>
-                <div className="branch-row branch-row--bottom">
-                    <div className="circle-group circle-group--bottom">
-                         <div className="circle"></div>
-                         <div className="circle"></div>
-                    </div>
-                    <div className="circle-group circle-group--bottom">
-                        <div className="circle"></div>
-                        <div className="circle"></div>
-                    </div>
-                </div>
-            </section>
-        </main>
-    )
+    return <h1>Home</h1>
 };
 
 export default Home;
