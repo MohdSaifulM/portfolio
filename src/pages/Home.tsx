00a2adaa897d7c857efb950c09e9478b786ae85b
SaifM <mohd.saiful@live.com>
2023-01-08 16:16:07 +0800

    style: 💄 Added text and avatar to homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,19 @@
 const Home = () => {
-    return <h1>Home</h1>
+    return (
+        <main>
+            <div className="section section-home-header">
+                <h1 className="heading-home">
+                    <strong>Passionate, discerning and focused</strong> with an
+                    industrial automation background. Seeking fulfilment through
+                    bettering lives and progressively delivering solutions &
+                    convenience as a software engineer.
+                </h1>
+            </div>
+            <div className="section section-home-avatar">
+                <img src="src\assets\avatar.png" alt="avatar" />
+            </div>
+        </main>
+    );
 };
 
 export default Home;
