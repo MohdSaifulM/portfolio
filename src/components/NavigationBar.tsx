f65591be40c5d70c05b28509cac7b1506d986065
SaifM <mohd.saiful@live.com>
2023-01-09 22:20:47 +0800

    style: 💄 Added NavigationBar component

---
+++ src/components/NavigationBar.tsx
@@ -0,0 +1,24 @@
+const NavigationBar = () => {
+    return (
+        <div className="menu">
+            <nav className="menu-nav">
+                <ul>
+                    <li>
+                        About
+                    </li>
+                    <li>
+                        Contact
+                    </li>
+                    <li>
+                        Journey
+                    </li>
+                    <li>
+                        Projects
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default NavigationBar;
