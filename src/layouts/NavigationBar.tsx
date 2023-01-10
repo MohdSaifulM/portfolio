19a95038e642a9c3071130ac808813136574ac10
SaifM <mohd.saiful@live.com>
2023-01-10 23:28:20 +0800

    style: 💄 Added animations for navigation buttons

--- src/layouts/NavigationBar.tsx
+++ src/layouts/NavigationBar.tsx
@@ -3,10 +3,10 @@ const NavigationBar = () => {
         <div className="menu">
             <nav className="menu__nav">
                 <ul className="menu__list">
-                    <li className="menu__item">About</li>
-                    <li className="menu__item">Contact</li>
-                    <li className="menu__item">Journey</li>
-                    <li className="menu__item">Projects</li>
+                    <li className="menu__item"><a href="#" className="menu__link">About</a></li>
+                    <li className="menu__item"><a href="#" className="menu__link">Contact</a></li>
+                    <li className="menu__item"><a href="#" className="menu__link">Journey</a></li>
+                    <li className="menu__item"><a href="#" className="menu__link">Projects</a></li>
                 </ul>
             </nav>
         </div>
