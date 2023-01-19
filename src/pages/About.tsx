a50b04995b098b107b4206334bcfe7be61badfad
SaifM <mohd.saiful@live.com>
2023-01-19 20:27:01 +0800

    style: 💄 html for about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,18 @@
 const About = () => {
     return (
         <main>
+            <div className="section section-about">
+                <div className="about">
+                    <figure className="about__shape">
+                        <img src="src\assets\avatar.png" alt="" className="about__img" />
+                        <figcaption className="about__caption">Saif M</figcaption>
+                    </figure>
+                    <div className="about__text">
+                        <h3>About Me</h3>
+                        <p>Passionate, discerning and focused with an industrial automation background. Seeking fulfilment through bettering lives and progressively delivering solutions & convenience as a software engineer</p>
+                    </div>
+                </div>
+            </div>
         </main>
     );
 };
