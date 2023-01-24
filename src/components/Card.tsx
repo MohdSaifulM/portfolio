69e79e047f2e67b5d9f1cdc87f032e2f2e23b5ce
SaifM <mohd.saiful@live.com>
2023-01-24 20:50:29 +0800

    refactor: 💡 restructure card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,26 +1,18 @@
 const Card = () => {
     return (
-        <div className="card">
-            <div className="card__side card__side--front">
-                <div className="card__picture card__picture--1">
-                    &nbsp;
-                </div>
-                <h4 className="card__heading">
-                    <span className="card__heading-span card__heading-span--1">
-                        Frontend Development
-                    </span>
-                </h4>
-            </div>
-            <div className="card__side card__side--back card__side--back-1">
-                <div className="card__details">
-                    <ul>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                    </ul>
-                </div>
+        <div className="card-container">
+            <div className="card">
+                <a href="">
+                    <div className="card--display">
+                        <i></i>
+                        <h2></h2>
+                    </div>
+                    <div className="card--hover">
+                        <h2></h2>
+                        <p></p>
+                    </div>
+                </a>
+                <div className="card--border"></div>
             </div>
         </div>
     );
